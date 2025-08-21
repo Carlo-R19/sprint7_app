3b2ac7aae23dc7f603c7b6e53f3dcbeb351061e0
Carlo-R19 <carlo.ramirezsan@gmail.com>
2025-08-20 19:34:10 -0600

    Actualización de app.py

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 # Aplicación para desplegar información de vehículos a la venta
 
-import pandas 
-import streamlit 
-import plotly_express
+import pandas as pd
+import streamlit as st
+import plotly_express as px
 
-car_data = pandas.read_csv(r'C:\Users\Carlo\Documents\TripleTen\Jupyter\Directorio Principal\sprint7_app\sprint7_app\vehicles_us.csv') # Lee los datos del csv
-hist_button = streamlit.button('Crear histograma') # Crea un botón 
+st.header('Venta de vehículos', divider = 'blue')
 
-streamlit.header('Venta de vehículos')
 
-if hist_button:
-    streamlit.write('Creación de un histograma para el conjunto de anuncios de venta de vehículos')
-    fig = plotly_express.histogram(car_data, x = 'odometer')
-    streamlit.plotly_chart(fig, use_container_width=True)
+car_data = pd.read_csv(r'C:\Users\Carlo\Documents\TripleTen\Jupyter\Directorio Principal\sprint7_app\sprint7_app\vehicles_us.csv') # Lee los datos del csv
+#hist_button = st.button('Crear histograma') # Crea un botón para crear un histograma
+#disp_button = st.button('Crear gráfico de dispersión') # Crear un botón para hacer un gráfico de dispersión
 
-print(car_data)
+build_hist = st.checkbox('Construir histograma')
+buld_disp = st.checkbox('Construir gráfico de dispersión')
+
+# opciones = list(df.columns)[0:6]
+
+#v = st.multiselect(
+ #   label = 'Seleccione 2 variables',
+ #   options = opciones, 
+ #   max_selections= 2
+#)
+
+if build_hist:
+    st.write('Creación de un histograma para el conjunto de anuncios de venta de vehículos')
+    fig_hist = px.histogram(car_data, x = 'odometer')
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+if buld_disp:
+    st.write('Creación de un gráfico de dispersión para el conjunto de anuncuios de venta de vehículos')
+    fig_disp = px.scatter(car_data, x = 'odometer', y = 'price')
+    st.plotly_chart(fig_disp)

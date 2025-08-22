96e686ecb04679b60937d9f6a624161d49002e6f
Carlo-R19 <carlo.ramirezsan@gmail.com>
2025-08-21 19:42:08 -0600

    Actualización de app.py para mostrar gráficos correctamente

--- app.py
+++ app.py
@@ -11,23 +11,26 @@ car_data = pd.read_csv(r'C:\Users\Carlo\Documents\TripleTen\Jupyter\Directorio P
 #hist_button = st.button('Crear histograma') # Crea un botón para crear un histograma
 #disp_button = st.button('Crear gráfico de dispersión') # Crear un botón para hacer un gráfico de dispersión
 
-build_hist = st.checkbox('Construir histograma')
-buld_disp = st.checkbox('Construir gráfico de dispersión')
+build_hist = st.button('Construir histograma')
+buld_disp = st.button('Construir gráfico de dispersión')
 
-# opciones = list(df.columns)[0:6]
-
-#v = st.multiselect(
- #   label = 'Seleccione 2 variables',
- #   options = opciones, 
- #   max_selections= 2
-#)
+opciones_disp = list(car_data.columns)[0:9]
 
 if build_hist:
-    st.write('Creación de un histograma para el conjunto de anuncios de venta de vehículos')
-    fig_hist = px.histogram(car_data, x = 'odometer')
+    st.write('Creación de un histograma para el conjunto de anuncios de venta de vehículos de acuerdo al odometro') 
+    fig_hist = px.histogram(car_data, x = 'odometer') 
     st.plotly_chart(fig_hist, use_container_width=True)
 
+v_d = st.multiselect(
+    label = 'Seleccione las variables', 
+    options = opciones_disp,
+    max_selections = 2
+)
+
 if buld_disp:
-    st.write('Creación de un gráfico de dispersión para el conjunto de anuncuios de venta de vehículos')
-    fig_disp = px.scatter(car_data, x = 'odometer', y = 'price')
-    st.plotly_chart(fig_disp)
+    try: 
+        st.write(f'Creación de un gráfico de dispersión {v_d[1]} vs {v_d[0]}') 
+        fig_disp = px.scatter(car_data, x = v_d[0], y = v_d[1], title = f'Dispersión {v_d[1]} vs {v_d[0]}') 
+        st.plotly_chart(fig_disp)
+    except:
+        st.write('Selecciona las variables')
